250c219c86f7f2827ecb9d9991119ba57ffaf117
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2023-10-10 12:11:49 +0700

    little experiment

--- MarvelSnap/Program.DisplaySpectre.cs
+++ MarvelSnap/Program.DisplaySpectre.cs
@@ -8,10 +8,42 @@ public partial class Program
 {
 	static void DisplaySpectre(MarvelSnapGame game, Player player1, Player player2) 
 	{
-		AnsiConsole.Write(MarvelSnapLayout());
+		//AnsiConsole.Write(MarvelSnapLayout());
+		// while (true) 
+		// {
+		// 	var key = Console.ReadKey(true).Key;
+		// 	//Console.WriteLine(key);
+		// 	Console.Clear();
+		// 	AnsiConsole.Write(MarvelSnapLayout(key.ToString()));
+		// }
+		Test();
 	}
 	
-	static Table MarvelSnapLayout() 
+	static void Test()
+	{
+		var (row, column) = (0, 0);
+		while (true) {
+			var key = Console.ReadKey(true).Key;
+			Console.Clear();
+			switch (key) {
+				case ConsoleKey.UpArrow:
+					row = row <= 0 ? 2 : row - 1;
+					break;
+				case ConsoleKey.DownArrow:
+					row = row >= 2 ? 0 : row + 1;
+					break;
+				case ConsoleKey.LeftArrow:
+					column = column <= 0 ? 2 : column - 1;
+					break;
+				case ConsoleKey.RightArrow:
+					column = column >= 2 ? 0 : column + 1;
+					break;
+			}
+			Console.WriteLine((row, column));
+		}
+	}
+	
+	static Table MarvelSnapLayout(string key) 
 	{
 		var cards = new Table()
 			.Centered()
@@ -23,14 +55,17 @@ public partial class Program
 		StringBuilder sbLocation1 = new();
 		sbLocation1.AppendLine();
 		sbLocation1.AppendLine("Onslaught's Citadel");
+		sbLocation1.Append("[[■]]");
 		
 		StringBuilder sbLocation2 = new();
 		sbLocation2.AppendLine();
 		sbLocation2.AppendLine("Dream Dimension");
+		sbLocation2.Append($"[[{key}]]");
 		
 		StringBuilder sbLocation3 = new();
 		sbLocation3.AppendLine();
 		sbLocation3.AppendLine("Kyln");
+		sbLocation3.Append("[[ ]]");
 		
 		var location1 = new Panel(sbLocation1.ToString());
 		var location2 = new Panel(sbLocation2.ToString());
