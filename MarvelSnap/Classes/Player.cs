98e673ba05603d0b6ea97d7afa315dd8af10268b
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2023-10-18 22:29:03 +0700

    implement IPlayer interface

--- MarvelSnap/Classes/Player.cs
+++ MarvelSnap/Classes/Player.cs
@@ -1,28 +1,18 @@
 ﻿namespace MarvelSnap;
 
-public class IPlayer
+public class Player : IPlayer
 {
 	public int Id { get; set; }
 	public string? Name { get; set; }
 
-	public IPlayer(int id)
+	public Player(int id)
 	{
 		Id = id;
 	}
 
-	public IPlayer(int id, string name)
+	public Player(int id, string name)
 	{
 		Id = id;
 		Name = name;
-	}
-
-	public override int GetHashCode()
-	{
-		return Id;
-	}
-
-	public override bool Equals(object? obj)
-	{
-		return obj is IPlayer player && player.Id == Id;
 	}
 }
---
+++ MarvelSnap/Interfaces/IPlayer.cs
@@ -0,0 +1,7 @@
+﻿namespace MarvelSnap;
+
+public interface IPlayer
+{
+    int Id { get; set; }
+    string? Name { get; set; }
+}
